0cf6e42a83aec99c95b6d31dea8a2f2016d622b0
Hunar Roop Kahlon <hunar.roop@gmail.com>
2017-12-27 11:41:59 -0800

    in a hacky way
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- scripts/x86_64-unknown-linux-cirs.sh
+++ scripts/x86_64-unknown-linux-cirs.sh
@@ -5,5 +5,7 @@ set -ex
 
 gcc --version
 rustc -vV
+ln -s "$TRAVIS_BUILD_DIR/x86_64-unknown-linux-cirs.json" "$TRAVIS_BUILD_DIR/scripts/x86_64-unknown-linux-cirs.json"
 xargo build --verbose --target x86_64-unknown-linux-cirs
 xargo test --verbose --target x86_64-unknown-linux-cirs
+rm "$TRAVIS_BUILD_DIR/scripts/x86_64-unknown-linux-cirs.json"
